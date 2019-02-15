8fa74b6815a55c2fa8d769e58d5f7a374b834531
davidfred1994 <fred_david239@yahoo.com>
2019-02-15 00:38:40 -0800

    done

--- app/app.js
+++ app/app.js
@@ -2,7 +2,6 @@
 Init app
 interact with DOM
 interact with localstorage
-
  */
 
 $(document).ready(function(){
@@ -10,8 +9,7 @@ $(document).ready(function(){
   //var keyData = 'ourKey'; // going to need to make this dynamic?
 
 
-  $('.btn-add').on('click', function(e){
-    console.log(e);
+  $('.btn-add').on('click', function(){
     var keyData = $('.input-key').val();
     var valueData = $('.input-value').val();
     // write to db
@@ -23,22 +21,20 @@ $(document).ready(function(){
     // <div class="display-data-item" data-keyValue="keyData">valueData</div>
     // if you use backticks ` you can use ${templateLiterals}
     // TODO make this vars make sense across the app
-    $('.container-data').html('<div class="display-data-item" data-keyValue="'+ keyData +'">'+valueData+'</div>');
     $('.input-key').val('');
     $('.input-value').val('');
   });
 
 
   // update db
-    // need to expand when  more than 1 item is added
-
-  // delete item
-  $('.container-data').on('click', '.display-data-item', function(e){
-    console.log(e.currentTarget.dataset.keyvalue);
-    var keyData = e.currentTarget.dataset.keyvalue;
-    localStorage.removeItem(keyData);
+  $(".btn-update").on('click', function(){
     $('.container-data').text('');
-  });
+    for (var i = 0; i < localStorage.length; i++){
+      console.log(localStorage.getItem(localStorage.key(i)));
+      $('.container-data').append('<div class="display-data-item">'+'Day '+localStorage.key(i)+': '+localStorage.getItem(localStorage.key(i))+'</div>');
+    }
+ });
+
   // delete all?
   $('.btn-clear').click(function(){
     localStorage.clear();
